3120cc4c5de6d0bb638c6b3533eeecadb119055b
Ruben-nn <60340371+Ruben-nn@users.noreply.github.com>
2020-09-03 16:58:08 +0200

    tower update
    blyat

---
+++ D5 Game Project/Assets/Ruben/Scripts/BaseTower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseTower : MonoBehaviour
+{
+    public float detectionRange;
+    public float attackRate;
+
+    public Transform enemy;
+    private void Start()
+    {
+        //GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+    private void Update()
+    {
+
+    }
+}
--- D5 Game Project/Assets/Ruben/Scripts/RDTower.cs
+++ D5 Game Project/Assets/Ruben/Scripts/RDTower.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RDTower : MonoBehaviour
+public class RDTower : BaseTower
 {
+
     // Start is called before the first frame update
     void Start()
     {
