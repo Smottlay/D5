c8864751010d87474e73723c696ff808cdfa6a2f
Sammt <97067288@st.deltion.nl>
2020-09-18 13:47:52 +0200

    Particle Progress
    Added Fire Particle Images

---
+++ D5 Game Project/Assets/Chris/Particles/PartySystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartySystem : MonoBehaviour
+{
+    public ParticleSystem[] particleSystemies;
+
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ActivateAllParticles();
+        }
+
+        void ActivateAllParticles()
+        {
+            particleSystemies[0].Play();
+            particleSystemies[1].Play();
+        }
+    }
+}
