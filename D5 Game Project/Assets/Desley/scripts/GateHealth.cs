4e701e256e98de51f6f6ae51e25b07d33c5a4ac7
Desleyo <desleyoord@gmail.com>
2020-09-21 12:45:50 +0200

    healthbar gate
    fuck you chris
    healthbar added

---
+++ D5 Game Project/Assets/Desley/scripts/GateHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GateHealth : MonoBehaviour
+{
+    public GameObject cam;
+
+    public Canvas canvas;
+    public Slider slider;
+
+    public float health;
+    public float currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = cam.GetComponent<PlayerHealth>().health;
+        slider.maxValue = 50;
+        slider.value = slider.maxValue;
+        currentHealth = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = cam.GetComponent<PlayerHealth>().health;
+
+        if (currentHealth > health)
+        {
+            slider.value = health;
+            currentHealth = health;
+        }
+        transform.LookAt(transform.position + cam.transform.forward);
+        canvas.transform.LookAt(transform.position + cam.transform.forward);
+    }
+}
--- D5 Game Project/Assets/Desley/scripts/PlayerHealth.cs
+++ D5 Game Project/Assets/Desley/scripts/PlayerHealth.cs
@@ -8,6 +8,8 @@ public class PlayerHealth : MonoBehaviour
 {
     public GameObject endPanel;
     public GameObject particleObject;
+    public GameObject wholeGate;
+    public GameObject brokenGate;
 
     public int health;
     public bool alive;
@@ -17,6 +19,8 @@ public class PlayerHealth : MonoBehaviour
     {
         Time.timeScale = 1;
         particleObject = GameObject.FindGameObjectWithTag("DoorExplosion");
+        wholeGate.SetActive(true);
+        brokenGate.SetActive(false);
         alive = true;
         health = 50;
         endPanel.SetActive(false);
@@ -28,6 +32,8 @@ public class PlayerHealth : MonoBehaviour
         if(health <= 0 && alive)
         {
             particleObject.GetComponent<ParticlePlayer>().ParticleStart();
+            wholeGate.SetActive(false);
+            brokenGate.SetActive(true);
             endPanel.SetActive(true);
             alive = false;
             Time.timeScale = 0.25f;
