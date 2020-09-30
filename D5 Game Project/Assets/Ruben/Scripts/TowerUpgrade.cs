b9ba387f549b72d2ead98357edfe1ac50c932905
Ruben-nn <60340371+Ruben-nn@users.noreply.github.com>
2020-09-30 16:23:32 +0200

    upgrade menu
    added upgrade menu

---
+++ D5 Game Project/Assets/Ruben/Scripts/TowerUpgrade.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TowerUpgrade : MonoBehaviour
+{
+    private Renderer rend;
+    public Color hoverColor;
+    public Color startColor;
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+
+    private void OnMouseDown()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+    }
+}
