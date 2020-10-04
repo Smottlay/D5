6c06e09dbb51988a9e4954384e0e3bf1c509b2c7
Smottlay <53128377+Smottlay@users.noreply.github.com>
2020-10-04 18:28:02 +0200

    Particles/Shaders
    Added Mist Particle
    Added Moth Particle
    Added Enemy Dissolve (WIP)

---
+++ D5 Game Project/Assets/Chris/Shaders/DissolveOverTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveOverTime : MonoBehaviour
+{
+    public Material dissolveMaterial;
+
+    void Start()
+    {
+        dissolveMaterial.SetFloat("_TimeSlider", Time.deltaTime);
+    }
+}
