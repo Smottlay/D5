40a5aadd971e378f68b15eeaed6b72769b56214c
Desleyo <desleyoord@gmail.com>
2020-10-09 14:16:20 +0200

    begin transition mission

---
+++ D5 Game Project/Assets/Desley/scripts/Transition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    public Animator transition;
+    public GameObject cam;
+    public GameObject spawner;
+    public GameObject timePanel;
+
+    public void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        spawner = GameObject.FindGameObjectWithTag("spawner");
+        timePanel = GameObject.FindGameObjectWithTag("timePanel");
+        cam.GetComponent<PlayerMovement>().setSensitivity(0);
+        spawner.SetActive(false);
+        timePanel.SetActive(false);
+    }
+
+    public void EndTransition()
+    {
+        transition.SetTrigger("End");
+        cam.GetComponent<PlayerMovement>().setSensitivity(1);
+        spawner.SetActive(true);
+        timePanel.SetActive(true);
+    }
+}
---
+++ D5 Game Project/Assets/Desley/scripts/TransitionText.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class TransitionText : MonoBehaviour
+{
+    public GameObject MissionPanel;
+    public Text MissionNameText;
+    public TextMeshProUGUI MissionLoreText;
+    public string[] missionName;
+
+    [TextArea(15, 20)]
+    public string[] missionLore;
+    public float typingSpeed;
+
+    Scene currentScene;
+    public string sceneName;
+
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+
+        if (sceneName == ("Main Scene Dawg"))
+        {
+            MissionNameText.text = missionName[0];
+            StartCoroutine(TypeFirstLore());
+        }
+        else
+        {
+            MissionNameText.text = missionName[1];
+            StartCoroutine(TypeSecondLore());
+        }
+    }
+    
+    IEnumerator TypeFirstLore()
+    {
+        foreach(char letter in missionLore[0].ToCharArray())
+        {
+            MissionLoreText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+    IEnumerator TypeSecondLore()
+    {
+        foreach (char letter in missionLore[1].ToCharArray())
+        {
+            MissionLoreText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StopAllCoroutines();
+            gameObject.GetComponent<Transition>().EndTransition();
+        }
+    }
+}
