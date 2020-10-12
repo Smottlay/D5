9674409f0cb8ffbf1212e1dac11e4b11bb46dae5
Smottlay <53128377+Smottlay@users.noreply.github.com>
2020-10-12 02:26:27 +0200

    UI
    Added Radio UI
    Updated Ingame UI

---
+++ D5 Game Project/Assets/Chris/UX/UIFlicker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIFlicker : MonoBehaviour
+{
+    public float flashTime;
+    public float flashDuration;
+    public float holdTime;
+    public float holdReset;
+    public Image signalDark;
+    //public bool toggleFlash;
+
+    public void Start()
+    {
+        signalDark.enabled = true;
+    }
+    public void Update()
+    {
+         flashTime += flashDuration;
+         if (flashTime >= 5f)
+         {
+            signalDark.enabled = false;
+            holdTime -= Time.deltaTime;
+            if (holdTime <= 0f)
+            {
+                flashTime = 0f;
+                holdTime = holdReset;
+            }
+            
+         }
+        else
+        {
+            signalDark.enabled = true;
+        }
+        
+    }
+}
