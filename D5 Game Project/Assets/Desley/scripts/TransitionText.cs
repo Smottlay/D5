b4b9bc5c713f35fee080d7dc9fa691ffbe3aa332
Desleyo <desleyoord@gmail.com>
2020-10-14 18:54:19 +0200

    creep sounds fixed

--- D5 Game Project/Assets/Desley/scripts/TransitionText.cs
+++ D5 Game Project/Assets/Desley/scripts/TransitionText.cs
@@ -7,9 +7,6 @@ using TMPro;
 
 public class TransitionText : MonoBehaviour
 {
-    public AudioSource level1;
-    public AudioSource level2;
-
     public GameObject MissionPanel;
     public Text MissionNameText;
     public TextMeshProUGUI MissionLoreText;
@@ -32,13 +29,11 @@ public class TransitionText : MonoBehaviour
 
         if (sceneName == ("Level 1"))
         {
-            level1.Play();
             MissionNameText.text = missionName[0];
             StartCoroutine(TypeFirstLore());
         }
         else if(sceneName ==  ("Level 2"))
         {
-            level2.Play();
             MissionNameText.text = missionName[1];
             StartCoroutine(TypeSecondLore());
         }
@@ -69,8 +64,6 @@ public class TransitionText : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            level1.Stop();
-            level2.Stop();
             StopAllCoroutines();
             gameObject.GetComponent<Transition>().EndTransition();
             activeTimer = 1f;
