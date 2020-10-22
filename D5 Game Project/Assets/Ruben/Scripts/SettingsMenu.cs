6b35e11ab4ad728092d8e1f2afbddba460832efc
Desleyo <desleyoord@gmail.com>
2020-10-22 19:40:39 +0200

    fixed major bug
    fixed non placement bug lvl 1

--- D5 Game Project/Assets/Ruben/Scripts/SettingsMenu.cs
+++ D5 Game Project/Assets/Ruben/Scripts/SettingsMenu.cs
@@ -28,10 +28,7 @@ public class SettingsMenu : MonoBehaviour
         for(int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + "x" + resolutions[i].height;
-            if (!options.Contains(option))
-            {
                 options.Add(option);
-            }
 
             if(resolutions[i].width == Screen.currentResolution.width &&
                 resolutions[i].height == Screen.currentResolution.height)
